01800dbe3aee85e025654ed977d69341622cf7a2
Adel <adel.chatraei78@gmail.com>
2026-08-01 14:28:48 -0700

    refactor: minor cleanup in cart and checkout pages

--- app/(main)/cart/page.tsx
+++ app/(main)/cart/page.tsx
@@ -1,17 +1,16 @@
-import { ShieldCheck, ShoppingCart } from "lucide-react";
+import CartContent from "@/components/cart-page/CartContent";
+import CartHeader from "@/components/cart-page/CartHeader";
 
 const Cart = () => {
-    return;
+    return (
+        <div className="min-h-screen bg-[#f5f7fb] px-4 py-6 sm:px-6 lg:px-10">
+            <div className="mx-auto max-w-7xl space-y-5">
+                <CartHeader />
+
+                <CartContent />
+            </div>
+        </div>
+    );
 };
 
 export default Cart;
-
-/////////////
-
-// const Cart = () => {
-//     return <div >
-
-//     </div>;
-// };
-
-// export default Cart;
--- app/(main)/support/page.tsx
+++ app/(main)/support/page.tsx
@@ -1,16 +1,5 @@
-import CartContent from "@/components/cart-page/CartContent";
-import CartHeader from "@/components/cart-page/CartHeader";
-
 const Support = () => {
-    return (
-        <div className="min-h-screen bg-[#f5f7fb] px-4 py-6 sm:px-6 lg:px-10">
-            <div className="mx-auto max-w-7xl space-y-5">
-                <CartHeader />
-
-                <CartContent />
-            </div>
-        </div>
-    );
+    return <div>support page</div>;
 };
 
 export default Support;
