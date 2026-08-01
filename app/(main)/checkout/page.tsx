9f8269edd0f16b25b9186b48996b74da61804567
Adel <adel.chatraei78@gmail.com>
2026-08-01 15:20:34 -0700

    save checkout page

--- app/(main)/checkout/page.tsx
+++ app/(main)/checkout/page.tsx
@@ -1,5 +1,16 @@
+import CheckoutContent from "@/components/checkout-page-cmp/CheckoutContent";
+import CheckoutHeader from "@/components/checkout-page-cmp/CheckoutHeader";
+
 const page = () => {
-    return <div>checkout page</div>;
+    return (
+        <div className="min-h-screen bg-[#f5f7fb] px-4 py-6 sm:px-6 lg:px-10">
+            <div className="mx-auto max-w-7xl space-y-5">
+                <CheckoutHeader />
+
+                <CheckoutContent />
+            </div>
+        </div>
+    );
 };
 
 export default page;
